c3f6b23da1a7f50e077f358647f6a1b830cb032b
loc <loc@mail.com>
2025-03-03 17:32:04 +0700

    LocationService: add create and find locations function

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Location } from './entities/location';
 
 @Injectable()
-export class LocationService {}
+export class LocationService {
+  constructor(
+    @InjectRepository(Location)
+    private readonly locationRepository: Repository<Location>,
+  ) {}
+
+  create(locationData: Partial<Location>) {
+    const location = this.locationRepository.create(locationData);
+    return this.locationRepository.save(location);
+  }
+
+  findAll() {
+    return this.locationRepository.find({
+      relations: ['parent', 'children'],
+    });
+  }
+
+  findOne(id: string) {
+    return this.locationRepository.findOne({
+      where: { id },
+      relations: ['parent', 'children'],
+    });
+  }
+}
